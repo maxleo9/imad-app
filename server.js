1d010d2ae6e41dbb739487d83ab73052d5d27943
maxleo9 <31630533+maxleo9@users.noreply.github.com>
2017-09-07 00:32:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', ''));
 });
 
 
 
 
-app.get('https://www.facebook.com/images/photos/profile/gradient.png', function (req, res) {
+app.get('/ui/https://www.facebook.com/images/photos/profile/gradient.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'https://www.facebook.com/images/photos/profile/gradient.png'));
 });
 
