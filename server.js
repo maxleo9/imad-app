86755c1402e8e8ea093c17c5056334a914180841
maxleo9 <31630533+maxleo9@users.noreply.github.com>
2017-09-06 22:16:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/árticle-one', function(req,res) {
+    res.send('Árticle one requested and will be served here');
+    });
+    
+    app.get('/árticle-two', function(req,res) {
+    res.send('Árticle one requested and will be served here');
+    });
+    
+    app.get('/árticle-three', function(req,res) {
+    res.send('Árticle one requested and will be served here');
+    });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
